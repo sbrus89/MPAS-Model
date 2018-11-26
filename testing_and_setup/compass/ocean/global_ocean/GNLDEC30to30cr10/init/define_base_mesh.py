d10e0ccba0c25369f38b18da774c98e351da9f4b
Steven Brus <sbrus@lanl.gov>
2018-11-26 15:33:39 -0700

    Add Greenland mesh files in compass

---
+++ testing_and_setup/compass/ocean/global_ocean/GNLDEC30to30cr10/init/define_base_mesh.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import numpy as np
+import coastal_tools as ct
+
+
+def cellWidthVsLatLon():
+
+    km = 1000.0
+
+    params = ct.default_params
+    params["dx_min_coastal"] = 20.0*km
+    params["trans_width"] = 600.0*km
+    params["trans_start"] = 400.0*km
+    params["mesh_type"] = "EC"
+    params["dx_max_global"] = 120.0*km
+
+    params["plot_box"] = np.array([-140,0,30,90]) 
+
+    #print "***Gulf Coast***"
+    #params["region_box"] = ct.US_Gulf_Coast
+    #params["restrict_box"] = ct.Gulf_restrict
+    #cell_width, lon, lat = ct.coastal_refined_mesh(params)
+
+    #print "***East Coast***"
+    #params["region_box"] = ct.US_East_Coast
+    #params["restrict_box"] = ct.East_Coast_restrict
+    #cell_width,lon,lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+    #print "***Caribbean***"
+    #params["region_box"] = ct.Caribbean
+    #params["restrict_box"] = ct.Caribbean_restrict
+    #params["trans_width"] = 400.0*km
+    #params["trans_start"] = 300.0*km
+    #params["n_longest"] = 50
+    #cell_width,lon,lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+
+
+    params["origin"] = np.array([-70,73])
+
+    print "***Northern Canada***"
+    params["region_box"] = {"include":[np.array([-126.5,-59,67,85])],
+                            "exclude":[]}
+    params["restrict_box"] = ct.Empty
+    cell_width, lon, lat = ct.coastal_refined_mesh(params)
+
+    print "***Newfoundland***" 
+    Newfoundland = {"include":[np.array([-65.0,-50.0,53.0,60.0])],
+                    "exclude":[]}
+    params["region_box"] = Newfoundland 
+    params["restrict_box"] = ct.Empty
+    params["trans_width"] = 600.0*km
+    params["trans_start"] = 400.0*km
+    cell_width,lon,lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+    print "***Greenland***"
+    Greenland = {"include":[np.array([-81.5,-12.5,60,85])],
+                 "exclude":[np.array([[-87.6,58.7],
+                                      [-51.9,56.6],
+                                      [-68.9,75.5], 
+                                      [-107.0,73.3]]),
+                            np.array([[-119.0,74.5],
+                                      [-92.7,75.9],
+                                      [-52.84,83.25],
+                                      [-100.8,84.0]]),
+                            np.array([[-101.3,68.5],
+                                      [-82.4,72.3],
+                                      [-68.7,81.24],
+                                      [-117.29,77.75]]),
+                            np.array([-25.0,-10.0,62.5,67.5])]}  
+    params["region_box"] = Greenland 
+    params["restrict_box"] = ct.Empty
+    cell_width, lon, lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+    print "***Bering Straight****"
+    params["lon_wrap"] = True
+    params["region_box"] = {"include":[np.array([-172.5,-165,65,66.5])],
+                            "exclude":[]} 
+    cell_width,lon,lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+    print "***Greenland***"
+    Greenland = {"include":[np.array([[-66.06,71.33],
+                                      [-46.36,57.87],
+                                      [-25.45,65.36],
+                                      [-12.76,74.20]])],
+                 "exclude":[]}
+    params["region_box"] = Greenland 
+    params["restrict_box"] = ct.Empty
+    params["dx_min_coastal"] = 10.0*km
+    params["trans_width"] = 300.0*km
+    params["trans_start"] = 100.0*km
+    params["lon_wrap"] = False  
+    cell_width, lon, lat = ct.coastal_refined_mesh(params,cell_width,lon,lat)
+
+    return cell_width / km, lon, lat
